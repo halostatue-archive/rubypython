7eef9e5423d7bd1ffa238f6ce3d693f4c3fa3db2
Zach Raines <achatesavc@gmail.com>
2008-08-07 11:09:35 -0400

    Increasing test coverage for extension

--- test/test_rubypython_bridge_extn.rb
+++ test/test_rubypython_bridge_extn.rb
@@ -3,28 +3,58 @@ require "test/unit"
 $:.unshift File.dirname(__FILE__) + "/../ext/rubypython_bridge"
 require "rubypython_bridge.so"
 
-class TestRubypythonBridgeExtn < Test::Unit::TestCase
+class TestRubyPythonBridgeExtn < Test::Unit::TestCase
   
   def test_func_with_module
     pickle_return=RubyPythonBridge.func("cPickle","loads","(dp1\nS'a'\nS'n'\ns(I1\nS'2'\ntp2\nI4\ns.")
     assert_equal(pickle_return,{"a"=>"n", [1, "2"]=>4})
   end
   
-  def test_module_delegation
+  def test_start_stop
+    assert(RubyPythonBridge.start)
+    assert(!RubyPythonBridge.start)
+    assert(RubyPythonBridge.stop)
+    assert(!RubyPythonBridge.stop)
+  end
+  
+  def test_instance_wrapper
+    
+  end
+end
+
+class TestRubyPythonBridgeWithCPickle < Test::Unit::TestCase
+  def setup
     RubyPythonBridge.start
-    cPickle=RubyPythonBridge.import("cPickle")
-    assert_instance_of(RubyPythonBridge::RubyPyModule,cPickle)
-    assert_equal(cPickle.loads("(dp1\nS'a'\nS'n'\ns(I1\nS'2'\ntp2\nI4\ns."),{"a"=>"n", [1, "2"]=>4})
-    dumped_array=cPickle.dumps([1,2,3,4])
-    assert_equal(cPickle.loads(dumped_array),[1,2,3,4])
-    assert_raise NoMethodError do
-      cPickle.splack
-    end
-    assert_instance_of(RubyPythonBridge::RubyPyClass,cPickle.PicklingError)
-    cPickle.free_pobj
+    @cPickle=RubyPythonBridge.import "cPickle"
+  end
+  
+  def teardown
     ObjectSpace.each_object(RubyPythonBridge::RubyPyObject) do |o|
       o.free_pobj
     end
-    assert(RubyPythonBridge.stop)
-  end  
+    RubyPythonBridge.stop
+  end
+  
+  
+  def test_data_passing
+    assert_equal(@cPickle.loads("(dp1\nS'a'\nS'n'\ns(I1\nS'2'\ntp2\nI4\ns."),{"a"=>"n", [1, "2"]=>4})
+    dumped_array=@cPickle.dumps([1,2,3,4])
+    assert_equal(@cPickle.loads(dumped_array),[1,2,3,4])
+  end
+  
+  def test_method_missing
+    assert_raise NoMethodError do
+      @cPickle.splack
+    end
+  end
+  
+  def test_class_wrapping
+    assert_instance_of(RubyPythonBridge::RubyPyClass,@cPickle.PicklingError)
+  end
+  
+  def test_module_method_wrapping
+    assert_instance_of(RubyPythonBridge::RubyPyModule,@cPickle)
+  end
+  
 end
+

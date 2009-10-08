c85e595aae965072e00ec04c4bfd0b831c7a91e6
Zach Raines <raineszm@gmail.com>
2009-10-08 01:26:35 -0400

    Cleaned up tests a little.

--- test/test_session.rb
+++ test/test_session.rb
@@ -1,6 +1,6 @@
 class TestSessionImport < Test::Unit::TestCase
   def test_session
     require 'rubypython/session'
-    assert RubyPython.import "cPickle"
+    assert(RubyPython.import("cPickle"), "Import failed during session test.")
   end
 end

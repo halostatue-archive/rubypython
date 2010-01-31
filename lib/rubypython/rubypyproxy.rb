2cd2148a92eeba208d1cd962d37e11189d537780
Zach Raines <raineszm@gmail.com>
2010-01-31 13:32:01 -0500

    Starting to write delegation methods for RubyPyProxy class.

--- lib/rubypython/rubypyproxy.rb
+++ lib/rubypython/rubypyproxy.rb
@@ -1,7 +1,12 @@
+class RubyPyApi::NullPObjectError < RuntimeError
+end
+
 class RubyPyApi::RubyPyProxy
 
+  attr_reader :pObject
+
   def initialize(pObject)
-    @pObject = pObejct
+    @pObject = pObject
   end
 
   def method_missing(name, *args, &block)
@@ -12,12 +17,26 @@ class RubyPyApi::RubyPyProxy
     args.map! do |arg|
       if(arg.instance_of? RubyPyApi::PyObject)
         arg
+      elsif(arg.instance_of?(RubyPyApi::RubyPyProxy))
+        if(arg.null?)
+          raise NullPObjectError("Null pObject pointer.")
+        else
+          arg.pObject
+        end
+             
       else
         RubyPyApi::PyObject.new(arg)
+      end
     end
 
-      
-      
+    pList = RubyPyApi::PyObject.newList(args)
+    pTuple = RubyPyApi::PyObject.makeTuple(pList)
+
+    pReturn = @pObject.callObject(pTuple)
+
+    return RubyPyProxy.new(pReturn)
   end
+      
+
 
 end

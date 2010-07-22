7913522fd4a22e951311836409a84b4efd0f2bec
Zach Raines <raineszm@gmail.com>
2010-07-21 20:51:25 -0400

    Adding files that were forgotten in second to last commit.
    --HG--
    branch : devel

---
+++ test/test_rubypython_sessions.rb
@@ -0,0 +1,25 @@
+
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class TestRubySessions < Test::Unit::TestCase
+  @@pickledObject = {1 => [0,1,2], 'STRING' => 4.9}
+
+
+  def test_cpickle_session
+    RubyPython.session do
+
+      cPickle = RubyPython.import 'cPickle'
+      data = File.open('test/fixture1.pickle'){|x| x.read}
+      depickledObject = cPickle.loads data
+      assert_equal(@@pickledObject,
+                  depickledObject.rubify,
+                  "Failed to correctly depickle an object")
+
+      assert_raise PythonError do 
+        cPickle.loads "INVALID STRING"
+      end
+    end
+
+  end
+
+end

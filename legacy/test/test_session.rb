61274af30b7129df592cd6bfe21677bbb4768def
Zach Raines <raineszm@gmail.com>
2010-08-07 15:44:00 -0400

    Removing some old legacy code that was kept for reference.
    --HG--
    branch : devel

--- legacy/test/test_session.rb
+++
@@ -1,6 +0,0 @@
-class TestSessionImport < Test::Unit::TestCase
-  def test_session
-    require 'rubypython/session'
-    assert(RubyPython.import("cPickle"), "Import failed during session test.")
-  end
-end

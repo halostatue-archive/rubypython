cddc24f9897bf5bbd3403ef42ec43d0f9fe5e99a
Zach Raines <raineszm@gmail.com>
2010-08-08 19:50:33 -0400

    Moving unit tests to rspec.
    --HG--
    branch : devel

--- spec/pyapi_spec.rb
+++ spec/pyapi_spec.rb
@@ -1,17 +1,23 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
 module TestConstants
-    @aString = "STRING"
-    @anInt = 1
-    @aChar = 'a'
-    @aFloat = 1.0
-    @anArray = [@anInt, @aChar, @aFloat, @aString]
-    @aHash = {
-      @anInt => @anInt,
-      @aChar.to_sym => @aChar,
-      @aSym => @aFloat,
-      @aString => @aString
+  #REDEFINE THESE SO THEY ARE VISIBILE
+    AString = "STRING"
+    AnInt = 1
+    AChar = 'a'
+    AFloat = 1.0
+    AnArray = [AnInt, AChar, AFloat, AString]
+    ASym = :sym
+    AHash = {
+      AnInt => AnInt,
+      AChar.to_sym => AChar,
+      ASym => AFloat,
+      AString => AString
     }
+    AConvertedHash = Hash[*AHash.map do |k, v|
+      key = k.is_a?(Symbol)? k.to_s : k
+      [key,v]
+    end.flatten]
 
 end
 
@@ -39,16 +45,56 @@ describe RubyPython::PyAPI::PyObject do
   describe "#new" do
 
     [
-      ["a string", @aString],
-      ["an int", @anInt],
-      ["a float", @aFloat],
-      ["an array", @anArray],
-      ["a symbol", @aSym],
-      ["a hash", @aHash]
+      ["a string", AString],
+      ["an int", AnInt],
+      ["a float", AFloat],
+      ["an array", AnArray],
+      ["a symbol", ASym],
+      ["a hash", AHash]
     ].each do |title, obj|
       it "should wrap #{title}" do
         described_class.new(obj).should be_instance_of(described_class)
       end
+    end
+
+
+  end #new
+
+  describe "#rubify" do
+
+    [
+      ["a string", AString],
+      ["an int", AnInt],
+      ["a float", AFloat],
+      ["an array", AnArray],
+      ["a symbol", ASym, ASym.to_s],
+      ["a hash", AHash, AConvertedHash]
+    ].each do |arr|
+      p arr
+      type, input, output = arr
+      output ||= input
+
+      it "should faithfully unwrap #{type}" do
+        described_class.new(input).rubify.should == output
+      end
+
+    end
+
+    #perhaps move this
+    it "should raise 'Unsupported' error when unable to convert object" do
+      lambda do
+        urllib2 = RubyPython::PyAPI.import 'urllib2'
+        request = urllib2.getAttr('Request')
+        request.rubify
+      end.should raise_exception(RubyPython::PyAPI::Conversion::UnsupportedConversion)
+    end
+
+  end #rubify
+
+  describe "#hasAttr" do
+    it "should return true when object has the requested attribute" do
+      pyStringModule = RubyPython::PyAPI.import("string")
+      pyStringModule.hasAttr("ascii_letters").should be_true
     end
 
   end
--- spec/rubypython_spec.rb
+++ spec/rubypython_spec.rb
@@ -1,11 +1,6 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
-  
-  it "find this spec in spec directory" do
-     violated "Be sure to write your specs"
-  end
+describe RubyPython do
+  it "should have some specs"
   
 end

602e079576541b174a608adf98593642760ec7d8
Zach Raines <raineszm@gmail.com>
2010-08-08 22:04:58 -0400

    Finished migrating specs for PyObject (I think)
    --HG--
    branch : devel

--- spec/pyapi_spec.rb
+++ spec/pyapi_spec.rb
@@ -36,6 +36,8 @@ describe RubyPython::PyAPI::PyObject do
   include TestConstants
   before do
     RubyPython::PyAPI.start
+    @string = RubyPython::PyAPI.import 'string'
+    @urllib2 = RubyPython::PyAPI.import 'urllib2'
   end
 
   after do
@@ -70,7 +72,6 @@ describe RubyPython::PyAPI::PyObject do
       ["a symbol", ASym, ASym.to_s],
       ["a hash", AHash, AConvertedHash]
     ].each do |arr|
-      p arr
       type, input, output = arr
       output ||= input
 
@@ -83,8 +84,7 @@ describe RubyPython::PyAPI::PyObject do
     #perhaps move this
     it "should raise 'Unsupported' error when unable to convert object" do
       lambda do
-        urllib2 = RubyPython::PyAPI.import 'urllib2'
-        request = urllib2.getAttr('Request')
+        request = @urllib2.getAttr('Request')
         request.rubify
       end.should raise_exception(RubyPython::PyAPI::Conversion::UnsupportedConversion)
     end
@@ -93,10 +93,92 @@ describe RubyPython::PyAPI::PyObject do
 
   describe "#hasAttr" do
     it "should return true when object has the requested attribute" do
-      pyStringModule = RubyPython::PyAPI.import("string")
-      pyStringModule.hasAttr("ascii_letters").should be_true
+      @string.hasAttr("ascii_letters").should be_true
     end
 
+    it "should return false when object does not have the requested attribute" do
+      @string.hasAttr("nonExistentThing").should be_false
+    end
+  end
+
+  describe "#getAttr" do
+    it "should fetch requested object attribute" do 
+      ascii_letters = @string.getAttr "ascii_letters"
+      ascii_letters.rubify.should == "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    end
+
+    it "should return a PyObject instance" do
+      ascii_letters = @string.getAttr "ascii_letters"
+      ascii_letters.should be_kind_of(described_class)
+    end
+  end
+
+  describe "#setAttr" do 
+    it "should modify the specified attribute of the object" do
+      pyNewLetters = described_class.new "RbPy"
+      @string.setAttr "ascii_letters", pyNewLetters
+      @string.getAttr("ascii_letters").rubify.should == pyNewLetters.rubify
+    end
+
+    it "should create the requested attribute if it doesn't exist" do 
+      pyNewString = described_class.new "python"
+      @string.setAttr "ruby", pyNewString
+      @string.getAttr("ruby").rubify.should == pyNewString.rubify
+    end
+  end
+
+  describe "#cmp" do
+
+    before do
+      @less = described_class.new 5
+      @greater = described_class.new 10
+      @less_dup = described_class.new 5
+    end
+
+    it "should return 0 when objects are equal" do
+      @less.cmp(@less_dup).should == 0
+    end
+
+    it "should change sign under interchange of arguments" do 
+      @less.cmp(@greater).should == -@greater.cmp(@less)
+    end
+
+    it "should return -1 when first object is less than the second" do
+      @less.cmp(@greater).should == -1
+    end
+    
+    it "should return 1 when first object is greater than the second" do
+      @greater.cmp(@less).should == 1
+    end
+  end
+
+  describe "#makeTuple" do
+    #Try to expand coverage here
+    it "should wrap single arguments in a tuple" do
+      arg = described_class.new AString
+      described_class.makeTuple(arg).rubify.should == [AString]
+    end
+  end
+
+  describe "#callObject" do
+    #Expand coverage types
+    it "should execute wrapped object with supplied arguments" do
+      arg = described_class.new AnInt
+      argt = described_class.makeTuple arg
+
+      builtin = RubyPython::PyAPI.import "__builtin__"
+      stringClass = builtin.getAttr "str"
+      stringClass.callObject(argt).rubify.should == AnInt.to_s
+    end
+  end
+
+  describe "#newList" do
+    it "should wrap supplied args in a Python list" do
+      args = AnArray.map do |obj|
+        described_class.new obj
+      end
+      described_class.newList(*args).rubify.should == AnArray
+    end
   end
 
 end

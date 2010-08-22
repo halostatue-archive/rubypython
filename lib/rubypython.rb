168702c778583a107a57bf8b95bbcc553b98389f
Zach Raines <raineszm@gmail.com>
2010-08-22 15:19:54 -0400

    Reorganized rubypython.rb to make it a bit less messy.
    --HG--
    branch : devel

--- lib/rubypython.rb
+++ lib/rubypython.rb
@@ -50,82 +50,77 @@ require 'rubypython/pymainclass'
 #    RubyPython.stop
 module RubyPython
 
-  @@legacy_mode = false
+  class << self
 
-  #Starts ups the Python interpreter. This method **must** be run
-  #before using any Python code. The only alternatives are use of the
-  #{session} and {run} methods.
-  #@return [Boolean] returns true if the interpreter was started here
-  #    and false otherwise
-  def self.start
-    if Python.Py_IsInitialized != 0
-      return false
+    #Determines whether RubyPython is operating in Normal Mode or Legacy Mode.
+    #If legacy_mode is true, RubyPython switches into a mode compatible with
+    #versions < 0.3.0. All Python objects returned by method invocations are
+    #automatically converted to natve Ruby Types if RubyPython knows how to do
+    #this. Only if no such conversion is known are the objects wrapped in proxy
+    #objects.  Other wise RubyPython automatically wraps all returned objects as
+    #an instance of {RubyPyProxy} or one of its subclasses.
+    #@return [Boolean]
+    attr_accessor :legacy_mode
+
+    #Starts ups the Python interpreter. This method **must** be run
+    #before using any Python code. The only alternatives are use of the
+    #{session} and {run} methods.
+    #@return [Boolean] returns true if the interpreter was started here
+    #    and false otherwise
+    def start
+      if Python.Py_IsInitialized != 0
+        return false
+      end
+      Python.Py_Initialize
+      true
     end
-    Python.Py_Initialize
-    true
-  end
 
-  #Stops the Python interpreter if it is running. Returns true if the
-  #intepreter is stopped by this invocation. All wrapped Python objects
-  #should be considered invalid after invocation of this method.
-  #@return [Boolean] returns true if the interpreter was stopped here
-  #    and false otherwise
-  def self.stop
-    if Python.Py_IsInitialized !=0
-      PyMain.main = nil
-      PyMain.builtin = nil
-      RubyPython::Operators.send :class_variable_set, '@@operator', nil
-      Python.Py_Finalize
-      return true
+    #Stops the Python interpreter if it is running. Returns true if the
+    #intepreter is stopped by this invocation. All wrapped Python objects
+    #should be considered invalid after invocation of this method.
+    #@return [Boolean] returns true if the interpreter was stopped here
+    #    and false otherwise
+    def stop
+      if Python.Py_IsInitialized !=0
+        PyMain.main = nil
+        PyMain.builtin = nil
+        RubyPython::Operators.send :class_variable_set, '@@operator', nil
+        Python.Py_Finalize
+        return true
+      end
+      false
     end
-    false
-  end
 
-  #Import a Python module into the interpreter and return a proxy object
-  #for it. This is the preferred way to gain access to Python object.
-  #@param [String] mod_name the name of the module to import
-  #@return [RubyPyModule] a proxy object wrapping the requested
-  #module
-  def self.import(mod_name)
-    pModule = Python.PyImport_ImportModule mod_name
-    pymod = PyObject.new pModule
-    if(PythonError.error?)
-      raise PythonError.handle_error
+    #Import a Python module into the interpreter and return a proxy object
+    #for it. This is the preferred way to gain access to Python object.
+    #@param [String] mod_name the name of the module to import
+    #@return [RubyPyModule] a proxy object wrapping the requested
+    #module
+    def import(mod_name)
+      pModule = Python.PyImport_ImportModule mod_name
+      pymod = PyObject.new pModule
+      if(PythonError.error?)
+        raise PythonError.handle_error
+      end
+      RubyPyModule.new(pymod)
     end
-    RubyPyModule.new(pymod)
-  end
 
-  #Switch RubyPython into a mode compatible with versions < 0.3.0. All
-  #Python objects returned by method invocations are automatically converted
-  #to natve Ruby Types if RubyPython knows how to do this. Only if no such
-  #conversion is known are the objects wrapped in proxy objects.
-  #@return [void]
-  def self.legacy_mode=(on_off)
-    @@legacy_mode = on_off
-  end
+    #Execute the given block, starting the Python interperter before its execution
+    #and stopping the interpreter after its execution. The last expression of the
+    #block is returned; be careful that this is not a Python object as it will
+    #become invalid when the interpreter is stopped.
+    #@param [Block] block the code to be executed while the interpreter is running
+    #@return the result of evaluating the given block
+    def session
+      start
+      yield
+    end
 
-  #Set RubyPython to automatically wrap all returned objects as an instance
-  #of {RubyPyProxy} or one of its subclasses.
-  #@return [Boolean]
-  def self.legacy_mode
-    @@legacy_mode
-  end
-
-  #Execute the given block, starting the Python interperter before its execution
-  #and stopping the interpreter after its execution. The last expression of the
-  #block is returned; be careful that this is not a Python object as it will
-  #become invalid when the interpreter is stopped.
-  #@param [Block] block the code to be executed while the interpreter is running
-  #@return the result of evaluating the given block
-  def self.session
-    start
-    yield
-  end
-
-  #The same as {session} except that the block is executed within the scope 
-  #of the RubyPython module.
-  def self.run(&block)
-    start
-    module_eval(&block)
+    #The same as {session} except that the block is executed within the scope 
+    #of the RubyPython module.
+    def run(&block)
+      start
+      module_eval(&block)
+    end
   end
 end

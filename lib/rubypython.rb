94d612b816e4d50d6496eb6dd600e017a083e57c
Zach Raines <raineszm@gmail.com>
2010-08-22 15:14:47 -0400

    Moved extension to core string class into core_ext subdirectory.
    --HG--
    branch : devel

--- lib/rubypython.rb
+++ lib/rubypython.rb
@@ -1,15 +1,8 @@
+require 'rubypython/core_ext/string'
 require 'rubypython/python'
 require 'rubypython/py_object'
 require 'rubypython/rubypyproxy'
 require 'rubypython/pymainclass'
-
-class String
-  #This is necessary for Ruby versions 1.8.6 and below as 
-  #String#end_with? is not defined in this case.
-  def end_with?(c)
-    self[-1].chr == c
-  end
-end unless String.respond_to? :end_with?
 
 
 #This module provides the direct user interface for the RubyPython extension.

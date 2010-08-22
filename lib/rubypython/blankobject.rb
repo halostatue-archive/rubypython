ab4f200abea8a3857a97f2fdc27bd2e2aecc6dbe
Zach Raines <raineszm@gmail.com>
2010-08-22 13:26:19 -0400

    Updated blankslate behavior to remove a warning in 1.9.1
    --HG--
    branch : devel

--- lib/rubypython/blankobject.rb
+++ lib/rubypython/blankobject.rb
@@ -5,13 +5,10 @@ module RubyPython
   #It is necessary to define this because no such class exists in Ruby
   #1.8.x
   class BlankObject < BlankSlate
-#    instance_methods.each do |m|
-#      undef_method m.to_sym unless m.to_s =~ /^(__)|(object_id)|(.*?$)/
-#    end
     class << self
       def hide(name)
         if instance_methods.include?(name) and
-          name.to_s !~ /^(__|instance_eval)/
+          name.to_s !~ /^(__|instance_eval|object_id)/
             @hidden_methods ||= {}
           @hidden_methods[name.to_sym] = instance_method(name)
           undef_method name

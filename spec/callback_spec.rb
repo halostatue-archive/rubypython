4a5b710bbdfb403cf5a60f40ff0a14f42242c450
Zach Raines <raineszm@gmail.com>
2011-01-23 12:31:04 -0500

    Updated callback spec to test returning of primitive ruby types.

--- spec/callback_spec.rb
+++ spec/callback_spec.rb
@@ -29,13 +29,24 @@ describe 'Callbacks' do
     end
   end
 
-  it 'should allow callbacks to return a ruby type' do
-    callback = Proc.new do 
-      true
-    end
+  [
+    ["an int", AnInt],
+    ["a float", AFloat],
+    ["a string", AString],
+    ["an array", AnArray],
+    ["an array", AnArray],
+    ["a hash", AConvertedHash],
+    ["true", true],
+    ["false", false],
+    ["nil", nil]
+  ].each do |rb_type, rb_value|
+    it "should allow callbacks to return #{rb_type}" do
+      callback = Proc.new do 
+        rb_value
+      end
 
-    @objects.apply_callback(callback, []).should be_true
-    
+      @objects.apply_callback(callback, []).should == rb_value
+    end
   end
 
 end

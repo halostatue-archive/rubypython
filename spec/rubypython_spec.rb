ea589c857cd357898f4cc926b58897815b7fc1ac
Zach Raines <raineszm@gmail.com>
2011-03-09 10:26:06 -0500

    Small bugfix on specs to handle tests which should not have the interpreter started in a before block.

--- spec/rubypython_spec.rb
+++ spec/rubypython_spec.rb
@@ -21,60 +21,63 @@ describe RubyPython do
   end
 end
 
-describe RubyPython, "#session" do
-  it "should start interpreter" do
-    RubyPython.session do
-      cPickle = RubyPython.import "cPickle"
-      cPickle.loads("(dp1\nS'a'\nS'n'\ns(I1\nS'2'\ntp2\nI4\ns.").rubify.should == {"a"=>"n", [1, "2"]=>4}
+describe RubyPython, :self_start => :true do
+
+  describe "#session" do
+    it "should start interpreter" do
+      RubyPython.session do
+        cPickle = RubyPython.import "cPickle"
+        cPickle.loads("(dp1\nS'a'\nS'n'\ns(I1\nS'2'\ntp2\nI4\ns.").rubify.should == {"a"=>"n", [1, "2"]=>4}
+      end
+    end
+
+    it "should stop the interpreter" do
+      RubyPython.session do
+        cPickle = RubyPython.import "cPickle"
+      end
+
+      RubyPython.stop.should be_false
     end
   end
 
-  it "should stop the interpreter" do
-    RubyPython.session do
-      cPickle = RubyPython.import "cPickle"
+  describe "#run" do
+    it "should start interpreter" do
+      RubyPython.run do
+        cPickle = import "cPickle"
+        cPickle.loads("(dp1\nS'a'\nS'n'\ns(I1\nS'2'\ntp2\nI4\ns.").rubify.should == {"a"=>"n", [1, "2"]=>4}
+      end
     end
 
-    RubyPython.stop.should be_false
-  end
-end
+    it "should stop the interpreter" do
+      RubyPython.run do
+        cPickle = import "cPickle"
+      end
 
-describe RubyPython, "#run" do
-  it "should start interpreter" do
-    RubyPython.run do
-      cPickle = import "cPickle"
-      cPickle.loads("(dp1\nS'a'\nS'n'\ns(I1\nS'2'\ntp2\nI4\ns.").rubify.should == {"a"=>"n", [1, "2"]=>4}
+      RubyPython.stop.should be_false
     end
   end
 
-  it "should stop the interpreter" do
-    RubyPython.run do
-      cPickle = import "cPickle"
+  describe '#reload_library', :slow => true do
+    it 'leaves RubyPython in a stable state' do
+      lambda do 
+        RubyPython.instance_eval { reload_library }
+        RubyPython.run {}
+      end.should_not raise_exception
+    end
+  end
+
+  describe '.configure', :slow => true do
+    it 'allows python executable to be specified', :unless => `which python2.6`.empty? do
+      RubyPython.configure :python_exe => 'python2.6'
+      RubyPython.run do
+        sys = RubyPython.import 'sys'
+        sys.version.rubify.to_f.should == 2.6
+      end
     end
 
-    RubyPython.stop.should be_false
-  end
-end
-
-describe RubyPython, '#reload_library', :slow => true do
-  it 'leaves RubyPython in a stable state' do
-    lambda do 
+    after(:all) do
+      RubyPython.clear_options
       RubyPython.instance_eval { reload_library }
-      RubyPython.run {}
-    end.should_not raise_exception
-  end
-end
-
-describe RubyPython, '.configure', :slow => true do
-  it 'allows python executable to be specified', :unless => `which python2.6`.empty? do
-    RubyPython.configure :python_exe => 'python2.6'
-    RubyPython.run do
-      sys = RubyPython.import 'sys'
-      sys.version.rubify.to_f.should == 2.6
     end
-  end
-
-  after(:all) do
-    RubyPython.clear_options
-    RubyPython.instance_eval { reload_library }
   end
 end

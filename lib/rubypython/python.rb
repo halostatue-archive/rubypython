526d93529ec8315c1c9d50071921f90986b0aa60
Aman Gupta <aman@tmm1.net>
2011-03-11 19:02:32 +0800

    Correctly convert strings with null bytes

--- lib/rubypython/python.rb
+++ lib/rubypython/python.rb
@@ -69,6 +69,8 @@ if RubyPython.loaded?
     # String Methods
     attach_function :PyString_AsString, [:pointer], :string
     attach_function :PyString_FromString, [:string], :pointer
+    attach_function :PyString_AsStringAndSize, [:pointer, :pointer, :pointer], :int
+    attach_function :PyString_FromStringAndSize, [:buffer_out, :ssize_t], :pointer
 
     # List Methods
     attach_function :PyList_GetItem, [:pointer, :int], :pointer
